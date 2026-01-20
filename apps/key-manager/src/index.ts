e676a7e61c5be710538fd644987043aa51870224
jxom <7336481+jxom@users.noreply.github.com>
2026-01-20 16:53:49 +1100

    chore: up

--- apps/key-manager/src/index.ts
+++ apps/key-manager/src/index.ts
@@ -2,5 +2,5 @@ import { env } from 'cloudflare:workers'
 import { Handler, Kv } from 'tempo.ts/server'
 
 export default Handler.keyManager({
-	kv: Kv.cloudflare(env.CREDENTIAL_STORE),
+	kv: Kv.cloudflare(env.KEY_STORE),
 })
